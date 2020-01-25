12d77c28de2699cf6df72fed757922581fb9ec06
Aleksandra <aviala@mail.ru>
2020-01-25 23:30:37 +0300

    Это наш первый commit

---
+++ script.js
@@ -0,0 +1,48 @@
+'use strict';
+
+a = 5;
+console.log (a);
+
+var leftBorderWidth = 1;
+let second =2;
+const pi =3.14;
+
+
+let person ={
+    name: "John", 
+    age : 25,
+    isMarried: false
+}
+
+console.log (person["name"]);
+
+let arr = ['plum.png','orange.jpg','apple.bmp'];
+
+console.log (arr[0]);
+
+//alert ("Hello world");
+
+//let answer = confirm ("Are you here?");
+///console.log (answer);
+
+// let answer = + prompt ("Are you 18?", "Да");
+// console.log (typeof(answer));
+
+
+// console.log ("arr" + "-object");
+// console.log (4 + +"-object");
+
+let incr = 10,
+    decr = 10;
+
+
+console.log (incr++);
+console.log (decr--);
+
+console.log (5%2);
+console.log("2" ===2);
+
+let isChecked = false,
+    isClosed = false;
+
+console.log (isChecked || !isClosed);
